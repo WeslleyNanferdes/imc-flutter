0fe2373e632569b9e6fcc59cf46d45d21b03262b
Weslley <devwaslley@gmail.com>
2024-05-22 11:38:40 -0300

    a

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 
 Color cinzaPagina = Colors.grey.shade700;
-Color cinzaBotaoPressionado = const Color.fromRGBO(117, 117, 117, 1);
-Color cinzaBotaoNaoPressionado = Colors.blue;
+Color cinzaBotaoPressionado = Colors.blue;
+Color cinzaBotaoNaoPressionado = Colors.grey.shade600;
+bool estadoButton = false;
 
 void main(){
   runApp(const MyApp());
@@ -38,23 +39,22 @@ class _MyApp extends State<MyApp>{
   }
 }
 
+
+
 class Button extends StatelessWidget{
   final String text;
-  static bool estadoButton = false;
   const Button({super.key, required this.text});
 
   @override
   Widget build(BuildContext context){
     return ElevatedButton(
       onPressed:() {
-        if (estadoButton == false){
-          estadoButton = true;
-        } else{
-          estadoButton = false;
-        } 
+        setState((){
+
+        });
       },
-      style: ElevatedButton.styleFrom(
-         backgroundColor: estadoButton ? cinzaBotaoPressionado : cinzaBotaoNaoPressionado,
+      style: ButtonStyle(
+        backgroundColor: MaterialStateProperty.all<Color>(estadoButton ? cinzaBotaoPressionado : cinzaBotaoNaoPressionado),
       ),
       child: Text(text),
     );
